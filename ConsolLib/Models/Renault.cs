8178690966ed3a3404f27c1f3690551bc48b95b2
LBozan <lbozan@outlook.com>
2016-08-03 16:16:16 +0300

    Create Renault.cs

---
+++ ConsolLib/Models/Renault.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+ 
+
+namespace JenericAndEventAndDelegate
+{
+   public class Renault : IAraclar
+    {
+        string model;
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+        string plaka;
+        public string Plaka
+        {
+            get
+            {
+                return plaka;
+            }
+            set
+            {
+                plaka = value;
+            }
+        }
+        string renk;
+        public string Renk
+        {
+            get
+            {
+                return renk;
+            }
+            set
+            {
+                renk = value;
+            }
+        }
+        bool kiradurum;
+        public bool KiraDurum
+        {
+            get
+            {
+                return kiradurum;
+            }
+            set
+            {
+                kiradurum = value;
+            }
+        }
+        int kirasay;
+        public int KiraSay
+        {
+            get
+            {
+                return kirasay;
+            }
+            set
+            {
+                kirasay = value;
+            }
+        }
+    }
+    
+}
